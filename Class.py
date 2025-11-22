302ad8b6c899722d83eee8911fd4040dd2605bb2
GaurangiSharma <gaurangi007@gmail.com>
2025-11-22 20:28:43 +0530

    Added Super()

--- Class.py
+++ Class.py
@@ -1,6 +1,5 @@
-# MULTIPLE Inheritance = Allows a class to inherit attributes and methods from another class
-#                         Helps with code reusability and extensibility
-#                         class Child(Parent)
+# MULTIPLE Inheritance
+#Super()
 
 class Animal:
     def __init__(self, name):
@@ -33,3 +32,62 @@ class Fish(Prey, Predator):
 rabbit = Rabbit("Bugs")
 hawk = Hawk("Tony")
 fish = Fish("Nemo")
+
+rabbit.eat()
+hawk.eat()
+fish.eat()
+print()
+rabbit.sleep()
+hawk.sleep()
+fish.sleep()
+
+print()
+print()
+# super() = Function used in a child class to call methods from a parent class (superclass).
+#                  Allows you to extend the functionality of the inherited methods
+
+class Shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+    def describe(self):
+        print(f"It is {self.color} and {'filled' if self.is_filled else 'not filled'}")
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+
+    def describe(self):
+        print(f"It is a circle with an area of {3.14 * self.radius * self.radius}cm^2")
+        super().describe()
+
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+    def describe(self):
+        print(f"It is a square with an area of {self.width * self.width}cm^2")
+        super().describe()
+
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+    def describe(self):
+        print(f"It is a triangle with an area of {self.width * self.height / 2}cm^2")
+        super().describe()
+
+circle = Circle(color="red", is_filled=True, radius=5)
+square = Square(color="blue", is_filled=False, width=6)
+triangle = Triangle(color="yellow", is_filled=True, width=7, height=8)
+
+circle.describe()
+print()
+square.describe()
+print()
+triangle.describe()
